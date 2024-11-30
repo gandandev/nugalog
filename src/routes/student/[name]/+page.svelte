10e00e0614074680e63a0f0d178ec0c74b15fdcb
gandan <188059221+gandandev@users.noreply.github.com>
2024-11-30 19:08:41 +0900

    새 기록 버튼 추가

--- src/routes/student/[name]/+page.svelte
+++ src/routes/student/[name]/+page.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
   import Log from '$lib/components/Log.svelte'
+  import Add from '~icons/mdi/add'
 
   import { data } from '$lib/stores'
 
   import { page } from '$app/stores'
 
-  let student = $derived($data.find(s => s.name === decodeURIComponent($page.params.name))!)
+  let student = $derived($data.find((s) => s.name === decodeURIComponent($page.params.name))!)
 </script>
 
 <div class="mx-auto min-w-[50%] space-y-1 overflow-y-auto p-12">
   {#each student.logs as log}
     <Log {log} />
   {/each}
+  <div>
+    <button
+      class="mx-auto mt-5 flex items-center gap-2 rounded-full px-4 py-2 text-stone-500 duration-150 hover:bg-stone-100 active:bg-stone-200"
+    >
+      <Add class="h-6 w-6" />
+      새 기록
+    </button>
+  </div>
 </div>
