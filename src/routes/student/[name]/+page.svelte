f34ade3c7c64327e2142b3743549dcc7d2bcb66b
gandan <188059221+gandandev@users.noreply.github.com>
2024-12-02 19:57:50 +0900

    기록 추가 기능 구현

--- src/routes/student/[name]/+page.svelte
+++ src/routes/student/[name]/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
   import Log from '$lib/components/Log.svelte'
+  import IconButton from '$lib/components/IconButton.svelte'
   import Add from '~icons/mdi/add'
+  import Close from '~icons/mdi/close'
+  import Check from '~icons/mdi/check'
 
-  import { data } from '$lib/stores'
+  import autosize from 'svelte-autosize'
+
+  import { data, type Log as LogType } from '$lib/stores'
 
   import { page } from '$app/stores'
 
@@ -19,6 +24,22 @@
       return s
     })
   }
+
+  let newLog: LogType | null = $state(null)
+
+  function saveNewLog() {
+    $data = $data.map((s) => {
+      if (s.name === student.name) {
+        return {
+          ...s,
+          logs: [...s.logs, newLog!]
+        }
+      }
+      return s
+    })
+
+    newLog = null
+  }
 </script>
 
 <div class="mx-auto min-w-[50%] space-y-1 overflow-y-auto p-12">
@@ -26,11 +47,33 @@
     <Log {log} deleteLog={() => deleteLog(i)} />
   {/each}
   <div>
-    <button
-      class="mx-auto mt-5 flex items-center gap-2 rounded-full px-4 py-2 text-stone-500 duration-150 hover:bg-stone-100 active:bg-stone-200"
-    >
-      <Add class="h-6 w-6" />
-      새 기록
-    </button>
+    {#if newLog}
+      <div class="flex flex-col gap-1">
+        <div class="mx-3 mt-2 flex items-center justify-between">
+          <input
+            type="date"
+            value={newLog.date.toISOString().slice(0, 10)}
+            oninput={(e) => (newLog!.date = new Date(e.currentTarget.value))}
+          />
+          <div class="flex items-center text-stone-500 duration-150 active:text-stone-600">
+            <IconButton Icon={Close} text="취소" onclick={() => (newLog = null)} />
+            <IconButton Icon={Check} text="저장" onclick={saveNewLog} />
+          </div>
+        </div>
+        <textarea
+          class="w-full resize-none rounded-lg bg-stone-50 p-3 duration-150 focus:outline-none"
+          bind:value={newLog.content}
+          use:autosize
+        ></textarea>
+      </div>
+    {:else}
+      <button
+        class="mx-auto mt-5 flex items-center gap-2 rounded-full px-4 py-2 text-stone-500 duration-150 hover:bg-stone-100 active:bg-stone-200"
+        onclick={() => (newLog = { date: new Date(), content: '' })}
+      >
+        <Add class="h-6 w-6" />
+        새 기록
+      </button>
+    {/if}
   </div>
 </div>
