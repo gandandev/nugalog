bcbfdb26cb4b01a48956299111dd11844ba858b3
gandan <188059221+gandandev@users.noreply.github.com>
2024-12-20 18:20:15 +0900

    tailwind에 desktop-first 적용

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,13 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
+    screens: {
+      // Desktop-first
+      xl: { max: '1279px' },
+      lg: { max: '1023px' },
+      md: { max: '767px' },
+      sm: { max: '639px' }
+    },
     extend: {
       fontFamily: {
         sans: ['Pretendard Variable', 'sans-serif'],
