4889e16172a49fada0bcef48709e9e074be3a506
gandan <188059221+gandandev@users.noreply.github.com>
2024-12-21 10:44:23 +0900

    /login 페이지 추가

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -1,1 +1,5 @@
-<h1 class="font-monasans text-2xl font-bold">nugalog</h1>
+<script lang="ts">
+  let { class: className } = $props<{ class?: string }>()
+</script>
+
+<h1 class="font-monasans text-2xl font-bold {className}">nugalog</h1>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Logo from '$lib/components/Logo.svelte'
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center">
+  <Logo class="text-4xl" />
+  Log in
+</div>
