30f17b2ae5f948b9abac56270172b443fec79979
gandan <188059221+gandandev@users.noreply.github.com>
2025-01-11 17:05:12 +0900

    일부 옵션 클릭 시 메뉴 닫기

--- src/lib/components/SidebarItem.svelte
+++ src/lib/components/SidebarItem.svelte
@@ -123,9 +123,14 @@
     show={showOptions}
     class="right-0 top-full mt-1 origin-top-right"
     options={[
-      { Icon: Edit, label: '이름 변경', onclick: () => (newName = student.name) },
-      { Icon: Keep, label: '상단 고정', onclick: togglePin },
-      { Icon: Delete, label: '학생 삭제', onclick: confirmdelete }
+      {
+        Icon: Edit,
+        label: '이름 변경',
+        onclick: () => (newName = student.name),
+        closeMenuOnClick: true
+      },
+      { Icon: Keep, label: '상단 고정', onclick: togglePin, closeMenuOnClick: true },
+      { Icon: Delete, label: '학생 삭제', onclick: confirmdelete, closeMenuOnClick: true }
     ]}
     button={optionsButton}
     closeMenu={() => (showOptions = false)}
