bd2ab44e4a025a7a2db69d34fe1b615c325f9f54
gandan <188059221+gandandev@users.noreply.github.com>
2025-01-12 19:38:24 +0900

    OpenGraph 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
   import '../app.css'
+  import { page } from '$app/stores'
 
   let { children } = $props()
 </script>
+
+<svelte:head>
+  <meta property="og:image" content="{$page.url.origin}/og-image.png" />
+</svelte:head>
 
 <div class="h-screen font-sans">
   {@render children()}
